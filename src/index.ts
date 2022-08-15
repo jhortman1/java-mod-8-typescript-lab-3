d2b96177530586ebe6960d23b741e2998ff190e1
James Hortman <jamesdhortman@gmail.com>
2022-08-15 16:21:26 -0400

    Created an HTML file that has 2 input fields to make the previous prepend() function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-// your code here
+  const button = document.getElementById("prepend") as HTMLButtonElement;
+  const input1 = document.getElementById("numSpaces") as HTMLInputElement;
+  const input2 = document.getElementById("stringToPrepend") as HTMLInputElement;
+
+    button!.addEventListener("click", function () {
+    console.log(prepend(input1!.value, input2!.value));
+    });
+
+function prepend(numSpaces: number | string, stringToPrepend: string):string {
+  let newString:string = "";
+  for(let i = 0; i<+numSpaces;i++)
+  {
+    newString+=" ";
+  }
+  return newString.concat(stringToPrepend);
+}
